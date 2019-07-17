a2dd74f5b4a9dc4263d56626e875e4e05c5b63bc
Administrator <Administrator@192.168.170.61>
2019-07-17 08:25:26 +0800

    commit a demo

---
+++ demo/src/demo/Demo.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Demo {
+
+	public Demo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void test() {
+		System.out.println("测试");
+	}
+	
+	private int age;
+	private String name;
+
+}
